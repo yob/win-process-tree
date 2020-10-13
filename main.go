064392a55019fd648d19bd6252aee2bfd8e52b96
James Healy <james@yob.id.au>
2020-10-14 08:45:08 +1100

    this works well for starting a 3-level process tree

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/binary"
+	"errors"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -52,37 +55,93 @@ func (g ProcessExitGroup) AddProcess(p *os.Process) error {
 		windows.Handle((*process)(unsafe.Pointer(p)).Handle))
 }
 
-func startTree(nesting int) *exec.Cmd {
-	//g, err := NewProcessExitGroup()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer g.Dispose()
 
+type JOBOBJECT_BASIC_PROCESS_ID_LIST struct {
+	NumberOfAssignedProcesses uint32
+	NumberOfProcessIdsInList  uint32
+	ProcessIdList [1024]byte
+}
+
+func (g ProcessExitGroup) ListProcesses() ([]uint64, error) {
+	JobObjectBasicProcessIdList := int32(3)
+	var list JOBOBJECT_BASIC_PROCESS_ID_LIST
+
+
+	err := windows.QueryInformationJobObject(
+		windows.Handle(g),
+	    JobObjectBasicProcessIdList,
+		uintptr(unsafe.Pointer(&list)),
+		uint32(unsafe.Sizeof(list)),
+		nil,
+	)
+	if err != nil {
+		panic(fmt.Sprintf("QueryInformationJobObject failed: %v", err))
+	}
+
+	if  list.NumberOfProcessIdsInList < list.NumberOfAssignedProcesses {
+		return nil, errors.New(fmt.Sprintf("JOBOBJECT_BASIC_PROCESS_ID_LIST buffer not large enough. Got %d pids, wanted %d", list.NumberOfProcessIdsInList, list.NumberOfAssignedProcesses))
+	}
+
+	pids := make([]uint64, list.NumberOfProcessIdsInList)
+	for i := 0; i < int(list.NumberOfProcessIdsInList); i++ {
+		pids[i] = binary.LittleEndian.Uint64(list.ProcessIdList[8*i : 8*(i+1)])
+	}
+	fmt.Printf("bnums: %v\n", pids)
+	return pids, nil
+}
+
+func startTree(nesting int) *exec.Cmd {
 	cmd := exec.Command(os.Args[0], strconv.Itoa(nesting - 1))
 	if err := cmd.Start(); err != nil {
 		log.Println("Start ERROR: %v", err)
 	}
 
-	//if err := g.AddProcess(cmd.Process); err != nil {
-	//	panic(err)
-	//}
 	return cmd
-
 }
 
 func main() {
 	argCount := len(os.Args)
-	if argCount != 2 {
-		panic("USAGE: foo <n>")
-	}
-	nesting, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		nesting = 1
+	nesting := 0
+	err := errors.New("fo")
+	if argCount == 1 { // # top of the process tree
+		nesting = 3
+	} else {
+		nesting, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			nesting = 1
+		}
 	}
 	log.Printf("nesting: %d\n", nesting)
 
-	if nesting >= 1 {
+	// only create a process group at the top of the tree, in theory
+	// all children will inherit it
+	if nesting == 3 {
+		g, err := NewProcessExitGroup()
+		if err != nil {
+			panic(err)
+		}
+		//defer g.Dispose()
+		cmd1 := startTree(nesting)
+		cmd2 := startTree(nesting)
+
+		if err := g.AddProcess(cmd1.Process); err != nil {
+			panic(err)
+		}
+
+		if err := g.AddProcess(cmd1.Process); err != nil {
+			panic(err)
+		}
+		time.Sleep(2 * time.Second)
+
+		g.ListProcesses()
+
+		if err := cmd1.Wait(); err != nil {
+			log.Println("cmd1 wait ERROR: %v", err)
+		}
+		if err := cmd2.Wait(); err != nil {
+			log.Println("cmd2 wait ERROR: %v", err)
+		}
+	} else if nesting >= 1 {
 		cmd1 := startTree(nesting)
 		cmd2 := startTree(nesting)
 
