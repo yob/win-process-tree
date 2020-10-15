5bf9d7adb968baa6a560fe209ba5f67ca9248792
James Healy <james@yob.id.au>
2020-10-15 13:47:37 +1100

    add ufave/cli and make the current behaviour a command
    ... so I can add more commands for different tree shapes

--- main.go
+++ main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
-	"errors"
 	"log"
 	"os"
 	"os/exec"
 	"strconv"
 	"time"
+
+	"github.com/urfave/cli/v2"
 )
 
 
+func main() {
+	app := &cli.App{
+		Name: "win-process-tree",
+		Usage: "Create various shaped process trees. Useful for testing process management code",
+		Commands: []*cli.Command{
+			{
+				Name:    "3tree",
+				Aliases: []string{"a"},
+				Usage:   "create a 3-level process tree that runs for about 30 seconds",
+				Action: func(c *cli.Context) error {
+					cmdThreeTree(c.Args().Get(0));
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+
 func startTree(nesting int) *exec.Cmd {
-	cmd := exec.Command(os.Args[0], strconv.Itoa(nesting - 1))
+	cmd := exec.Command(os.Args[0], "3tree", strconv.Itoa(nesting - 1))
 	if err := cmd.Start(); err != nil {
 		log.Println("Start ERROR: %v", err)
 	}
@@ -19,27 +45,25 @@ func startTree(nesting int) *exec.Cmd {
 	return cmd
 }
 
-func main() {
-	argCount := len(os.Args)
+
+func cmdThreeTree(argNesting string) error {
 	nesting := 0
-	err := errors.New("fo")
-	if argCount == 1 { // # top of the process tree
+
+	if argNesting == "" { // # top of the process tree
 		nesting = 3
 	} else {
-		nesting, err = strconv.Atoi(os.Args[1])
+		res, err := strconv.Atoi(argNesting)
 		if err != nil {
 			nesting = 1
 		}
+		nesting = res
 	}
 	log.Printf("nesting: %d\n", nesting)
 
-	// only create a process group at the top of the tree, in theory
-	// all children will inherit it
-	if nesting == 3 {
+	if nesting >= 1 {
 		cmd1 := startTree(nesting)
 		cmd2 := startTree(nesting)
-
-		time.Sleep(2 * time.Second)
+		cmd3 := startTree(nesting)
 
 		if err := cmd1.Wait(); err != nil {
 			log.Println("cmd1 wait ERROR: %v", err)
@@ -47,16 +71,10 @@ func main() {
 		if err := cmd2.Wait(); err != nil {
 			log.Println("cmd2 wait ERROR: %v", err)
 		}
-	} else if nesting >= 1 {
-		cmd1 := startTree(nesting)
-		cmd2 := startTree(nesting)
-
-		if err := cmd1.Wait(); err != nil {
-			log.Println("cmd1 wait ERROR: %v", err)
-		}
-		if err := cmd2.Wait(); err != nil {
-			log.Println("cmd2 wait ERROR: %v", err)
+		if err := cmd3.Wait(); err != nil {
+			log.Println("cmd3 wait ERROR: %v", err)
 		}
 	}
 	time.Sleep(10 * time.Second)
+	return nil
 }
