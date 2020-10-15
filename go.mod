e11505d49047373b90b570614f63cccd7abdc78c
James Healy <james@yob.id.au>
2020-10-15 15:21:38 +1100

    new orphans subcommand
    Intentionally orphan the leaf processes in a 3-level process tree,
    because that's a thing that's easy to do in windows

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/yob/go-windows
 
 go 1.15
 
-require github.com/urfave/cli/v2 v2.2.0
+require (
+	github.com/urfave/cli/v2 v2.2.0
+	golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211
+)
--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 	"time"
 
 	"github.com/urfave/cli/v2"
+	"golang.org/x/sys/windows"
 )
-
 
 func main() {
 	app := &cli.App{
@@ -25,6 +25,15 @@ func main() {
 					return nil
 				},
 			},
+			{
+				Name:    "orphans",
+				Aliases: []string{"a"},
+				Usage:   "create a 3-level process tree and then orphan the leaf processes",
+				Action: func(c *cli.Context) error {
+					cmdOrphans(c.Args().Get(0));
+					return nil
+				},
+			},
 		},
 	}
 
@@ -36,8 +45,8 @@ func main() {
 }
 
 
-func startTree(nesting int) *exec.Cmd {
-	cmd := exec.Command(os.Args[0], "3tree", strconv.Itoa(nesting - 1))
+func startTree(subcommand string, nesting int) *exec.Cmd {
+	cmd := exec.Command(os.Args[0], subcommand, strconv.Itoa(nesting - 1))
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Start(); err != nil {
@@ -63,9 +72,9 @@ func cmdThreeTree(argNesting string) error {
 	log.Printf("nesting: %d\n", nesting)
 
 	if nesting >= 1 {
-		cmd1 := startTree(nesting)
-		cmd2 := startTree(nesting)
-		cmd3 := startTree(nesting)
+		cmd1 := startTree("3tree", nesting)
+		cmd2 := startTree("3tree", nesting)
+		cmd3 := startTree("3tree", nesting)
 
 		if err := cmd1.Wait(); err != nil {
 			log.Println("cmd1 wait ERROR: %v", err)
@@ -78,5 +87,56 @@ func cmdThreeTree(argNesting string) error {
 		}
 	}
 	time.Sleep(10 * time.Second)
+	return nil
+}
+
+func terminatePid(pid uint32) error {
+	proc, err := windows.OpenProcess(windows.PROCESS_TERMINATE, false, pid)
+	if err != nil {
+		return err
+	}
+	err = windows.TerminateProcess(proc, 0)
+	windows.CloseHandle(proc)
+	return err
+}
+
+func cmdOrphans(argNesting string) error {
+	nesting := 0
+
+	if argNesting == "" { // # top of the process tree
+		nesting = 3
+	} else {
+		res, err := strconv.Atoi(argNesting)
+		if err != nil {
+			nesting = 1
+		}
+		nesting = res
+	}
+	log.Printf("orphan nesting: %d\n", nesting)
+
+	if nesting >= 1 {
+		cmd1 := startTree("orphans", nesting)
+		cmd2 := startTree("orphans", nesting)
+		cmd3 := startTree("orphans", nesting)
+
+		if err := cmd1.Wait(); err != nil {
+			log.Println("cmd1 wait ERROR: %v", err)
+		}
+		if err := cmd2.Wait(); err != nil {
+			log.Println("cmd2 wait ERROR: %v", err)
+		}
+		if err := cmd3.Wait(); err != nil {
+			log.Println("cmd3 wait ERROR: %v", err)
+		}
+		time.Sleep(10 * time.Second)
+	} else if nesting == 0 {
+		ppid := os.Getppid()
+		log.Printf("leaf process, ppid: %d\n", ppid)
+		err := terminatePid(uint32(ppid))
+		if err != nil {
+			log.Printf("Error killing ppid %d, %v", ppid, err)
+		}
+		time.Sleep(300 * time.Second)
+	}
 	return nil
 }
