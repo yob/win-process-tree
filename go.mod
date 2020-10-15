2fc3b0cb528a016c7d0fbee1d7f292dd3c9b4c3d
James Healy <james@yob.id.au>
2020-10-15 13:28:41 +1100

    strip back to a basic 3-level process tree with no Job

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/yob/go-windows
 
 go 1.15
-
-require golang.org/x/sys v0.0.0-20201009025420-dfb3f7c4e634
--- main.go
+++ main.go
@@ -1,94 +1,14 @@
 package main
 
 import (
-	"encoding/binary"
 	"errors"
-	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"strconv"
 	"time"
-	"unsafe"
-
-	"golang.org/x/sys/windows"
 )
 
-// We use this struct to retreive process handle(which is unexported)
-// from os.Process using unsafe operation.
-type process struct {
-	Pid    int
-	Handle uintptr
-}
-
-type ProcessExitGroup windows.Handle
-
-func NewProcessExitGroup() (ProcessExitGroup, error) {
-	handle, err := windows.CreateJobObject(nil, nil)
-	if err != nil {
-		return 0, err
-	}
-
-	info := windows.JOBOBJECT_EXTENDED_LIMIT_INFORMATION{
-		BasicLimitInformation: windows.JOBOBJECT_BASIC_LIMIT_INFORMATION{
-			LimitFlags: windows.JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE,
-		},
-	}
-	if _, err := windows.SetInformationJobObject(
-		handle,
-		windows.JobObjectExtendedLimitInformation,
-		uintptr(unsafe.Pointer(&info)),
-		uint32(unsafe.Sizeof(info))); err != nil {
-		return 0, err
-	}
-
-	return ProcessExitGroup(handle), nil
-}
-
-func (g ProcessExitGroup) Dispose() error {
-	return windows.CloseHandle(windows.Handle(g))
-}
-
-func (g ProcessExitGroup) AddProcess(p *os.Process) error {
-	return windows.AssignProcessToJobObject(
-		windows.Handle(g),
-		windows.Handle((*process)(unsafe.Pointer(p)).Handle))
-}
-
-
-type JOBOBJECT_BASIC_PROCESS_ID_LIST struct {
-	NumberOfAssignedProcesses uint32
-	NumberOfProcessIdsInList  uint32
-	ProcessIdList [1024]byte
-}
-
-func (g ProcessExitGroup) ListProcesses() ([]uint64, error) {
-	JobObjectBasicProcessIdList := int32(3)
-	var list JOBOBJECT_BASIC_PROCESS_ID_LIST
-
-
-	err := windows.QueryInformationJobObject(
-		windows.Handle(g),
-	    JobObjectBasicProcessIdList,
-		uintptr(unsafe.Pointer(&list)),
-		uint32(unsafe.Sizeof(list)),
-		nil,
-	)
-	if err != nil {
-		panic(fmt.Sprintf("QueryInformationJobObject failed: %v", err))
-	}
-
-	if  list.NumberOfProcessIdsInList < list.NumberOfAssignedProcesses {
-		return nil, errors.New(fmt.Sprintf("JOBOBJECT_BASIC_PROCESS_ID_LIST buffer not large enough. Got %d pids, wanted %d", list.NumberOfProcessIdsInList, list.NumberOfAssignedProcesses))
-	}
-
-	pids := make([]uint64, list.NumberOfProcessIdsInList)
-	for i := 0; i < int(list.NumberOfProcessIdsInList); i++ {
-		pids[i] = binary.LittleEndian.Uint64(list.ProcessIdList[8*i : 8*(i+1)])
-	}
-	fmt.Printf("bnums: %v\n", pids)
-	return pids, nil
-}
 
 func startTree(nesting int) *exec.Cmd {
 	cmd := exec.Command(os.Args[0], strconv.Itoa(nesting - 1))
@@ -116,24 +36,10 @@ func main() {
 	// only create a process group at the top of the tree, in theory
 	// all children will inherit it
 	if nesting == 3 {
-		g, err := NewProcessExitGroup()
-		if err != nil {
-			panic(err)
-		}
-		//defer g.Dispose()
 		cmd1 := startTree(nesting)
 		cmd2 := startTree(nesting)
 
-		if err := g.AddProcess(cmd1.Process); err != nil {
-			panic(err)
-		}
-
-		if err := g.AddProcess(cmd1.Process); err != nil {
-			panic(err)
-		}
 		time.Sleep(2 * time.Second)
-
-		g.ListProcesses()
 
 		if err := cmd1.Wait(); err != nil {
 			log.Println("cmd1 wait ERROR: %v", err)
