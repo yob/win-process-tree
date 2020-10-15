ae13b9eeb05daaabe5e09b3915f7e69f3a5e6612
James Healy <james@yob.id.au>
2020-10-15 15:44:58 +1100

    we don't need no stinking quotes

--- .buildkite/compile-and-run-orphans.bat
+++ .buildkite/compile-and-run-orphans.bat
@@ -1,4 +1,6 @@
 go build -o win-process-tree.exe .
-echo "This will create some orphan processes and then exit successfully."
-echo "Desired behaviour: the agent should record the build as sucessful and temrinate the orphan processes"
+
+echo This will create some orphan processes and then exit successfully.
+echo Desired behaviour: the agent should record the build as sucessful and temrinate the orphan processes
+
 win-process-tree.exe orphans
