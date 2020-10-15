28beedf0b465c3d13fae4f7a9c4b301273ce562a
James Healy <james@yob.id.au>
2020-10-15 15:43:13 +1100

    tweak build

--- .buildkite/compile-and-run-orphans.bat
+++ .buildkite/compile-and-run-orphans.bat
@@ -1,2 +1,4 @@
 go build -o win-process-tree.exe .
+echo "This will create some orphan processes and then exit successfully."
+echo "Desired behaviour: the agent should record the build as sucessful and temrinate the orphan processes"
 win-process-tree.exe orphans
