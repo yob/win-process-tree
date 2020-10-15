347f619674dd8f29f0452aa583c8f2bab9d0f528
James Healy <james@yob.id.au>
2020-10-15 15:47:05 +1100

    echo more context

--- .buildkite/compile-and-run-orphans.bat
+++ .buildkite/compile-and-run-orphans.bat
@@ -1,6 +1,7 @@
 go build -o win-process-tree.exe .
 
 echo This will create some orphan processes and then exit successfully.
-echo Desired behaviour: the agent should record the build as sucessful and temrinate the orphan processes
+echo Desired behaviour: the agent should record the build as sucessful and temrinate the orphan processes within 2 minutes
+echo If the build takes longer than that, it'll be cancelled. The child processes have tricked the agent into thinking the job is still running
 
 win-process-tree.exe orphans
