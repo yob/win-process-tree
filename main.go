2a41ada8da090ffff1b979af8cf1de06226f687b
James Healy <james@yob.id.au>
2020-10-15 13:16:39 +1100

    turn this into a mini-process bomb
    Create many nested processes down multiple process tree branches that
    complete quickly. Useful for testing race conditions in process
    management code.

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
-	"time"
+	//"time"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
@@ -121,26 +121,19 @@ func main() {
 			panic(err)
 		}
 		//defer g.Dispose()
-		cmd1 := startTree(nesting)
-		cmd2 := startTree(nesting)
+		for i := 1; i < 100; i++ {
+			cmd1 := startTree(nesting)
+			if err := g.AddProcess(cmd1.Process); err != nil {
+				panic(err)
+			}
 
-		if err := g.AddProcess(cmd1.Process); err != nil {
-			panic(err)
+			if err := cmd1.Wait(); err != nil {
+				log.Println("cmd1 wait ERROR: %v", err)
+			}
 		}
-
-		if err := g.AddProcess(cmd1.Process); err != nil {
-			panic(err)
-		}
-		time.Sleep(2 * time.Second)
 
 		g.ListProcesses()
 
-		if err := cmd1.Wait(); err != nil {
-			log.Println("cmd1 wait ERROR: %v", err)
-		}
-		if err := cmd2.Wait(); err != nil {
-			log.Println("cmd2 wait ERROR: %v", err)
-		}
 	} else if nesting >= 1 {
 		cmd1 := startTree(nesting)
 		cmd2 := startTree(nesting)
@@ -152,5 +145,5 @@ func main() {
 			log.Println("cmd2 wait ERROR: %v", err)
 		}
 	}
-	time.Sleep(10 * time.Second)
+	//time.Sleep(500 * time.Millisecond)
 }
